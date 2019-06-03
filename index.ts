824b47760d9fb17cefeb0a6f341d974e432d9e90
ZhouYuTao2017 <1619686166@qq.com>
2019-06-03 19:23:45 +0800

    feat: router引入

--- index.ts
+++ index.ts
@@ -1,7 +1,17 @@
+// 引入koa
 const Koa=require('koa');
+
+// 	启用koa2
 const app=new Koa();
+
+//引用文件模块
+const fs=require('fs');
+
+//自定义中间件
 const loggerGennerator=require('./middleware/logger-generator.js');
 
+//引入router
+const Router=require('koa-router');
 app.use(loggerGennerator())
 app.use(async (ctx)=>{
 	ctx.body='hello koa2'
