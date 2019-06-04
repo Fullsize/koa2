1a943c19f55580c0a09cf582e5b11beaee40af29
ZhouYuTao2017 <1619686166@qq.com>
2019-06-04 11:20:46 +0800

    路由

--- index.ts
+++ index.ts
@@ -1,20 +1,27 @@
 // 引入koa
-const Koa=require('koa');
+const Koa = require('koa');
 
-// 	启用koa2
-const app=new Koa();
+// 启用koa2
+const app = new Koa();
 
-//引用文件模块
-const fs=require('fs');
+// 引用文件模块
+const fs = require('fs');
 
-//自定义中间件
-const loggerGennerator=require('./middleware/logger-generator.js');
+// 自定义中间件
+const loggerGennerator = require('./middleware/logger-generator.js');
 
-//引入router
-const Router=require('koa-router');
+// 引入router
+const router = require('./routers/index.ts');
+
+// 加载中间件
 app.use(loggerGennerator())
-app.use(async (ctx)=>{
-	ctx.body='hello koa2'
-})
+
+// 启用路由
+app.use(router.routes())
+// router.allowedMethods()作用： 这是官方文档的推荐用法,
+app.use(router.allowedMethods());
+
+
+
 
 app.listen(3300);
